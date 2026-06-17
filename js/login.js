3bb045b74a4d60cd352d3c9b1d3d99c485c4dbb7
LoreA19 <loreacero19@gmail.com>
2026-06-17 10:50:02 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('loginForm');
     const cancelBtn = document.getElementById('cancelBtn');
-    const olvidóContraseña = document.getElementById('olvidadoContraseña');
+    const forgotPassword = document.getElementById('forgotPassword');
     const registerLink = document.getElementById('registerLink');
 
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
-
-        const nombreUsuario = document.getElementById('nombre usuario').value;
-        const contraseña = document.getElementById('contraseña').value;
-
-        if (nombreUsuario === 'Almirante' && contraseña === '1234') {
+        
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        
+        
+        if (username === 'adm' && password === '1234') {
+            window.location.href = './dashboard';
             window.location.href = './dashboard.html';
         } else {
             alert('Usuario o contraseña incorrectos.');
@@ -21,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
         loginForm.reset();
     });
 
-    olvidóContraseña.addEventListener('click', function(event) {
+    forgotPassword.addEventListener('click', function(event) {
         event.preventDefault();
         alert('Funcionalidad de recuperación de contraseña no implementada.');
     });
